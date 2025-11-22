b7d3e42f58f0fe1f24e76b712267d629feb730c4
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-11-21 23:50:06 -0500

    Fix build system issue that requires building twice after a patch change

--- src/main/register_patches.cpp
+++ src/main/register_patches.cpp
@@ -1,12 +1,16 @@
 #include "ovl_patches.hpp"
-#include "../../RecompiledPatches/patches_bin.h"
 #include "../../RecompiledPatches/recomp_overlays.inl"
 
 #include "librecomp/overlays.hpp"
 #include "librecomp/game.hpp"
 
+extern "C" {
+    extern const char bk_patches_bin[];
+    extern const size_t bk_patches_bin_size;
+}
+
 void banjo::register_bk_patches() {
-    recomp::overlays::register_patches(bk_patches_bin, sizeof(bk_patches_bin), section_table, ARRLEN(section_table));
+    recomp::overlays::register_patches(bk_patches_bin, bk_patches_bin_size, section_table, ARRLEN(section_table));
     recomp::overlays::register_base_exports(export_table);
     recomp::overlays::register_base_events(event_names);
     recomp::overlays::register_manual_patch_symbols(manual_patch_symbols);
