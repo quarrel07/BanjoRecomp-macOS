7fbb25464043aba44be447818d0acd8d0d39a1f8
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-12-30 21:58:54 -0500

    Bump version number and fix nav for note saving option

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -55,7 +55,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "0.1.0";
+const std::string version_string = "0.9.0";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
