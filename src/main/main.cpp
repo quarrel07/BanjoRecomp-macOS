3032db4c41f5a1c2026349cb467f49bb33779564
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2026-01-13 18:23:13 -0500

    Bump version to 1.0.0-rc1

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -64,7 +64,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "0.9.0";
+const std::string version_string = "1.0.0-rc1";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
