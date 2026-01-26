adf89fde21f63cfebccc4efaf2c514b6074bcf10
TSRStormed <107441806+TSRStormed@users.noreply.github.com>
2026-01-25 18:49:59 -0700

    Update lod_patches.c
    Add force high poly player bk

--- patches/lod_patches.c
+++ patches/lod_patches.c
@@ -1,5 +1,7 @@
 #include "patches.h"
 #include "core1/mlmtx.h"
+#include "functions.h"
+#include "enums.h"
 
 typedef struct {
     s32 cmd_0;
@@ -26,5 +28,27 @@ RECOMP_PATCH void func_80338B50(Gfx **gfx, Mtx **mtx, void *arg2) {
         if (cmd->min_C < dist && dist <= cmd->max_8) {
             func_80339124(gfx, mtx, (BKGeoList *)((s32)cmd + cmd->subgeo_offset_1C));
         }
+    }
+}
+
+// @recomp Patched to force the player Banjo & Kazooie model to always be high poly
+RECOMP_PATCH s32 func_802985F0(void){
+    switch(bsStoredState_getTransformation())
+    {
+        case TRANSFORM_2_TERMITE: 
+            return ASSET_34F_MODEL_BANJO_TERMITE;
+        case TRANSFORM_3_PUMPKIN: 
+            return ASSET_36F_MODEL_BANJO_PUMPKIN;
+        case TRANSFORM_5_CROC: 
+            return ASSET_374_MODEL_BANJO_CROC;
+        case TRANSFORM_4_WALRUS: 
+            return ASSET_359_MODEL_BANJO_WALRUS;
+        case TRANSFORM_6_BEE: 
+            return ASSET_362_MODEL_BANJO_BEE;
+        case TRANSFORM_7_WISHWASHY: 
+            return ASSET_356_MODEL_BANJO_WISHYWASHY;
+        // @recomp Force high poly BK player model on all maps
+        case TRANSFORM_1_BANJO: 
+            return ASSET_34E_MODEL_BANJOKAZOOIE_HIGH_POLY;
     }
 }
